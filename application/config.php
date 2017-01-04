0d61fb867dd0537a98c23ac907e5f755d4575a73
Jea杨 <jjonline@jjonline.cn>
2017-01-04 11:49:17 +0800

    应用结构规划

---
+++ application/config.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * 应用配置文件
+ * @authors Jea杨 (JJonline@JJonline.Cn)
+ * @date    2017-01-03 17:46:00
+ */
+
+return [
+    /**
+     * MySQL数据库配置部分
+     */
+    'db_host' => 'localhost',
+    'db_port' => 3306,
+    'db_user' => '',
+    'db_pwd'  => '',
+
+    /**
+     * Redis缓存配置部分
+     */
+    'redis_host' => '127.0.0.1',
+    'redis_port' => 6379,
+];
